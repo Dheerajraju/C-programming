decc9aab1534055f15d3a052a001bd0affc2f647
Dheeraj raju <dheshal07@gmail.com>
2025-09-27 16:54:06 +0530

    Update student.h

--- student record/student.h
+++ student record/student.h
@@ -1,7 +1,17 @@
-typedef struct student
-{
-int rollno;
-char name[20];
-float percentage;
-struct student *next;
+#define student.h
+
+typedef struct student{
+        char name[20];
+        int roll_no;
+        float mark;
+        struct student *next;
 } student;
+
+student *add(student *head);
+student *delete(student *head);
+void show(student *head);
+student modify(student *head);
+void save(student *head);
+student sort(student *head);
+student *delete_all(student *head);
+student *reverse(student *head);
